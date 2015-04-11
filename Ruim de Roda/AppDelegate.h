baff3a84c9d7c19ec251b9d80d19a91396a0a445
Moisés Pio <moises@Pios-MacBook.local>
2015-04-11 08:53:55 -0300

    Initial Commit

---
+++ Ruim de Roda/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Ruim de Roda
+//
+//  Created by Moisés Pio on 4/11/15.
+//  Copyright (c) 2015 Ruim de Roda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Ruim de Roda/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ruim de Roda
+//
+//  Created by Moisés Pio on 4/11/15.
+//  Copyright (c) 2015 Ruim de Roda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
