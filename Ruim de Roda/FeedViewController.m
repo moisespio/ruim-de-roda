dec85d5ddc4b9e02910974fe6417d179b69d8a32
Moisés Pio <moises@Pios-MacBook.local>
2015-04-11 19:18:17 -0300

    Interface da home

--- Ruim de Roda/FeedViewController.m
+++ Ruim de Roda/FeedViewController.m
@@ -28,7 +28,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    
+    self.tableView.separatorColor = [UIColor clearColor];
     [self loadData:nil];
 }
 
