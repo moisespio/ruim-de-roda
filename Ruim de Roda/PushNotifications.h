a16a050fb2abffa4cc057b071b659df0279fef84
Henrique Velloso <HenriqueVelloso@MacBook-Velloso.local>
2015-04-12 03:08:48 -0300

    Notifications 0.1

---
+++ Ruim de Roda/PushNotifications.h
@@ -0,0 +1,13 @@
+//
+//  PushNotifications.h
+//  Ruim de Roda
+//
+//  Created by Henrique Velloso on 4/12/15.
+//  Copyright (c) 2015 Ruim de Roda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PushNotifications : NSObject
+
+@end
