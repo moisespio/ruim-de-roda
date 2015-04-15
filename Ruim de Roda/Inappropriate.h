13af45da99ccc157ce0761db804b6feeb83cfafe
Matheus Frozzi Alberton <matheusfrozzi@MacBook-de-Frozzi.local>
2015-04-15 09:20:21 -0300

    commit to pull

---
+++ Ruim de Roda/Inappropriate.h
@@ -0,0 +1,26 @@
+//
+//  Report.h
+//  Flockr
+//
+//  Created by Matheus Frozzi Alberton on 06/04/15.
+//  Copyright (c) 2015 DevMac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Report.h"
+
+#import <Parse/Parse.h>
+
+@interface Inappropriate : NSObject
+
+- (void)addInappropriateQuery:(NSString *)optionID toReportId:(NSString *)reportID;
+- (void)requestInappropriate:(void (^)(NSArray *allReports, NSError *error))response;
+
+@property (strong, nonatomic) PFObject *pfObject;
+
+@property (strong, nonatomic) NSString *objectId;
+@property (strong, nonatomic) NSString *text;
+@property (strong, nonatomic) Report *report;
+@property (strong ,nonatomic) NSDate *createdAt;
+
+@end
---
+++ Ruim de Roda/Inappropriate.m
@@ -0,0 +1,51 @@
+//
+//  Report.m
+//  Flockr
+//
+//  Created by Matheus Frozzi Alberton on 06/04/15.
+//  Copyright (c) 2015 DevMac. All rights reserved.
+//
+
+#import "Inappropriate.h"
+
+@implementation Inappropriate
+
+- (void)addInappropriateQuery:(NSString *)optionID toReportId:(NSString *)reportID {
+    PFObject *reportObject = [PFObject objectWithClassName:@"Inappropriate"];
+    [reportObject setObject:[PFObject objectWithoutDataWithClassName:@"Report" objectId:reportID] forKey:@"report"];
+    [reportObject setObject:[PFObject objectWithoutDataWithClassName:@"Inappropriate_option" objectId:optionID] forKey:@"option"];
+//    [reportObject setObject:[PFObject objectWithoutDataWithClassName:@"User" objectId:userId] forKey:@"user"];
+
+    [reportObject saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (succeeded) {
+            NSLog(@"foi");
+        }
+    }];
+}
+
+- (void)requestInappropriate:(void (^)(NSArray *allReports, NSError *error))response {
+    PFQuery *inappropriateQuery = [PFQuery queryWithClassName:@"Inappropriate_option"];
+    [inappropriateQuery orderByDescending:@"createdAt"];
+
+    [inappropriateQuery findObjectsInBackgroundWithBlock:^(NSArray *resultsInappropriate, NSError *error) {
+        if (!resultsInappropriate) {
+            response(nil, error);
+        }
+        else {
+            NSMutableArray *allInappropriate = [[NSMutableArray alloc] init];
+            
+            for (PFObject *resultInappropriate in resultsInappropriate) {
+                // Report
+                Inappropriate *inappropriate = [[Inappropriate alloc] init];
+                inappropriate.objectId = resultInappropriate.objectId;
+                inappropriate.text = [resultInappropriate objectForKey:@"text"];
+                inappropriate.createdAt = resultInappropriate.createdAt;
+                
+                [allInappropriate addObject:inappropriate];
+            }
+            response([allInappropriate copy], nil);
+        }
+    }];
+}
+
+@end
